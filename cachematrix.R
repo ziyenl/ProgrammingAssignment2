3e5e15002a0d9241ec432f4d9cc130b0a8c7a4b7
ZI YEN LER <ZI YEN LER>
2015-12-27 09:37:38 +0000

    Adding Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  A pair of functions that cache the inverse of a matrix
 
 ##  This function creates a special "matrix" object that can cache its inverse
 
